0f336c73ca782109e9120dd97ec382970e9e2092
Keita Amadou <amadou.keita@etudiant.univ-reims.fr>
2020-10-16 13:53:32 +0200

    Ajout de avec traitement des majuscule  e02.js

--- e02.js
+++ e02.js
@@ -1,19 +1,16 @@
 function IsAPangrams(str){
     str = str.toLowerCase();
     let pangram = false;
-    let lstAsMaj = [];
     let lstAsMin = [];
     let ind = [];
     let count = 0;
     for (let i = 0; i < 25; i++)
     {
-        lstAsMaj.push(i+65);
         lstAsMin.push(i+97);
         
     }
     for (let i = 0; i < str.length; i++)
     {
-        let y = 0;
         const codeAs = str.charCodeAt(i);
         ind.push(codeAs);
     }
